a8b7639d5266e4e913850d19e37cba2c9c83e0ea
CEJ Van Niekirk <cejvaniekirk098@gmail.com>
2022-10-27 14:55:22 -0700

    added another TODO comment

--- test.py
+++ test.py
@@ -4,3 +4,4 @@
 # TODO add functionality too all menu commands
 # TODO finish advanced settings
 # TODO improve jumping from standard to advanced to currency all routes fixed
+# TODO add more currencies this weekend to dicts
