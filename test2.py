69fc8f192b6ef10db5647f4e70acbb02553585c3
CEJ Van Niekirk <cejvaniekirk098@gmail.com>
2022-11-04 22:06:26 -0700

    updates

--- test2.py
+++ test2.py
@@ -1,20 +1,7 @@
 import re
 
 
-value_to_calculate = "2 + "
-toBeCal = value_to_calculate.split(" ")
-measured = len(toBeCal)
-last_digit = re.search(r'\d+$', value_to_calculate)
+set_value = "2 * 2 +"
+last_digits = re.search(r'\d+$', set_value)
 
-
-
-        # ------------ Checks Addition Operator ------------ #
-        
-if toBeCal.count("+") and measured == 3 and last_digit != None:
-    first_num = toBeCal[0]
-    second_num = toBeCal[2]
-    result = int(first_num) + int(second_num)
-    # self.advanced_zero_base_input.set(value=str(result))
-    print(result)
-else:
-    print("None")
+print()
